566a08d317253466f0856cda4a71260c80888dca
Rodrigo Hernández <42217144+rodcko@users.noreply.github.com>
2021-02-27 09:35:52 -0600

    Composer

---
+++ bloques.php
@@ -0,0 +1,44 @@
+<?php
+if (condition) {
+  # code...
+} elseif (condition) {
+  # code...
+} else {
+  # code...
+}
+
+switch ($variable) {
+  case true:
+    # code...
+    break;
+  
+  case false:
+    # code...
+    break;
+
+  default:
+    # code...
+    break;
+}
+
+// Consulta de datos
+foreach ($datos as $row) {
+  # code...
+}
+
+$a = 1;
+while ($a <= 10) {
+  # code...
+
+  $a++;
+}
+
+// La condicion se hace al final, se hace si queremos que el codigo se ejecute al menos una vez
+do {
+  # code...
+} while ($a <= 10);
+
+
+for ($i = 0; $i < 10 ; $i++) { 
+  # code...
+}
--- index.php
+++ index.php
@@ -1,33 +1,5 @@
 <?php
-// Asignacion
-$num = 9;
-$lang = [
-  'es' => 'espanol',
-  'en' => 'ingles'
 
-];
+require __DIR__ . '/vendor/autoload.php';
 
-// Aritmetica
-echo "suma 2 + 2 " . ((int) 2 + (int) 2);
-echo "resta 2 - 2 " . ((int) 2 - (int) 2);
-echo "multiplica 2 * 2 " . 2 * 2;
-echo "divide 2 / 2 " . 2 / 2;
-echo "modulo 2 % 2 " . 2 % 2;
-echo "exponencial 2 ** 2 " . 2 ** 2;
-
-// Comparacion
-// Igual ==, valor '9' == 9
-// Igual ===, valor - tipo 9 === 9
-// Diferencias !=, valor
-// Diferencias !==, valor - tipo 
-// <, >, <= >=
-
-// Variables variables
-$app = 'name';
-$name = 'rodcko';
-
-echo $app; // name
-echo $$app; // rodcko
-
-// Sintaxis: forma correcta de escribir para que nuestro ordenador entienda...
-
+echo upper('hola');
