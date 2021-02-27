eb2a01133ded2e80a33423ba0d9e3e33de13479c
Rodrigo Hernández <42217144+rodcko@users.noreply.github.com>
2021-02-27 09:05:31 -0600

    Sintaxis basica de PHP: comparacion y variables

--- index.php
+++ index.php
@@ -15,5 +15,19 @@ echo "divide 2 / 2 " . 2 / 2;
 echo "modulo 2 % 2 " . 2 % 2;
 echo "exponencial 2 ** 2 " . 2 ** 2;
 
+// Comparacion
+// Igual ==, valor '9' == 9
+// Igual ===, valor - tipo 9 === 9
+// Diferencias !=, valor
+// Diferencias !==, valor - tipo 
+// <, >, <= >=
+
+// Variables variables
+$app = 'name';
+$name = 'rodcko';
+
+echo $app; // name
+echo $$app; // rodcko
+
 // Sintaxis: forma correcta de escribir para que nuestro ordenador entienda...
 
